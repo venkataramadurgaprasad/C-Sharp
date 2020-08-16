c61bdcb2ff789de83b57cbede3590c477ab01738
Kovvuri Venkata Rama Durgaprasad Reddy <69207735+venkataramadurgaprasad@users.noreply.github.com>
2020-08-16 19:12:53 +0530

    Added some functions and updated code.

--- Calculator.cs
+++ Calculator.cs
@@ -6,25 +6,32 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            int num_1,num_2,op,result=0;
+            //Declaration of all variables
+            int num_1,num_2,option,result=0;
             
+            //Choice to proceed for operations
             Console.WriteLine("Enter the option");
             Console.Write("1.\"+\"\t2.\"-\"\t3.\"x\"\t4.\"/\"\n");
-            op = Convert.ToInt32(Console.ReadLine());
             
-            if (op > 4 || op < 1)
+            //Input the option to perform corresponding operation
+            option = Convert.ToInt32(Console.ReadLine());
+            
+            //Exits if given any value else than options with a message
+            if (option > 4 || option < 1)
             {
-                Console.WriteLine("Enter Valid Option!...");
+                Console.WriteLine("Invalid Option.\nPlease Enter Valid Option!...");
                 System.Environment.Exit(1);
             }
             
+            //Input of 1st number
             Console.Write("Enter 1st Number: ");
             num_1 = Convert.ToInt32(Console.ReadLine());
             
+            //Input of 2nd number
             Console.Write("Enter 2nd Number: ");
             num_2 = Convert.ToInt32(Console.ReadLine());
             
-            switch(op)
+            switch(option)
             {
                 case 1 : result = num_1 + num_2;
                          break;
@@ -39,7 +46,12 @@ namespace Calculator
                          break;
             }
             
-            Console.WriteLine("The result is {0}",result);
+            //Prints the result
+            Console.WriteLine($"The result is {result}");
+            
+            //Waits for any input before exitting console
+            Console.WriteLine("Enter any key to exit console");
+            Console.ReadKey();
         }
     }
 }
